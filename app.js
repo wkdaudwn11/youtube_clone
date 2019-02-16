f8847e9b6b7bd39e8233f32e403553df4788a49c
Jang Myung Joo <wkdaudwn11@naver.com>
2019-02-16 10:50:46 +0900

    local Variable in Pug

--- app.js
+++ app.js
@@ -1,28 +1,33 @@
-
 import express from 'express';
 import morgan from 'morgan';
 import helmet from 'helmet';
 import cookieParser from 'cookie-parser';
 import bodyParser from 'body-parser';
 
+import {localsMiddleware} from "./middlewares"; // 미들웨어 임포트. 이건 pug에서 쓰임
+import routes from './routes'; //라우츠 순서 이쪽으로
+
 import userRouter from './router/userRouter';
 import globalRouter from './router/globalRouter';
 import videoRouter from './router/videoRouter';
-import routes from './routes'; 
+
+
 
 const app = express();
 
+app.use(helmet()); 
 app.set("view engine", "pug");
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 // 헬멧이랑 모간은 검색해볼것
-app.use(helmet()); 
 app.use(morgan("dev"));
 
 app.use(routes.home, globalRouter); // routes의 home변수가 호출되면 globalRouter를 호출
 app.use(routes.users, userRouter);
 app.use(routes.videos, videoRouter);
+
+app.use(localsMiddleware);
 
 export default app;
