08cb3d7efc425e13d8a4750434ad1d4395159d1d
Jang Myung Joo <wkdaudwn11@naver.com>
2019-02-16 10:57:58 +0900

    local Variable in Pug

--- app.js
+++ app.js
@@ -11,11 +11,9 @@ import userRouter from './router/userRouter';
 import globalRouter from './router/globalRouter';
 import videoRouter from './router/videoRouter';
 
-
-
 const app = express();
 
-app.use(helmet()); 
+app.use(helmet()); //헬멧도 맨위로.. 라우츠랑 헬멧 위치 바꾼 이유는 에러가 뜨길래. (안뜨면 안바꿔도 상관없음)
 app.set("view engine", "pug");
 app.use(cookieParser());
 app.use(bodyParser.json());
@@ -24,10 +22,12 @@ app.use(bodyParser.urlencoded({extended: true}));
 // 헬멧이랑 모간은 검색해볼것
 app.use(morgan("dev"));
 
+app.use(localsMiddleware);
+
 app.use(routes.home, globalRouter); // routes의 home변수가 호출되면 globalRouter를 호출
 app.use(routes.users, userRouter);
 app.use(routes.videos, videoRouter);
 
-app.use(localsMiddleware);
+
 
 export default app;
--- middlewares.js
+++ middlewares.js
@@ -1,6 +1,6 @@
 import routes from "./routes";
 
-export const localMiddleware = (req, res, next) => {
+export const localsMiddleware = (req, res, next) => {
     res.locals.siteName = "Youtube_Clone"; // 모든 페이지의 제목을 이걸로 사용하기 위함.
     res.locals.routes = routes;
     next();
